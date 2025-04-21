df9c3243328018d8389fcaf3006ee92766ea1cc5
ridho-goo <saratsidiq@gmail.com>
2025-04-21 23:48:29 +0700

    Add files via upload

---
+++ pertemuan3/MasterPBOMobil.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan3;
+
+/**
+ *
+ * @author Acer n15c1
+ */
+
+public class MasterPBOMobil {
+    public static void main(String[] args) {
+        PBOMobil mobil = new PBOMobil("Toyota", "Hitam", 180);
+        mobil.nyalakanMesin();
+        mobil.tampilkanInfo();
+    }
+}
---
+++ pertemuan3/MasterPBOplayer.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan3;
+
+/**
+ *
+ * @author Acer n15c1 
+ */
+
+public class MasterPBOplayer {
+    public static void main(String[] args) {
+        PBOplayer player = new PBOplayer("Tejo", 10, 100);
+        player.serang();
+        player.tampilkanStatus();
+    }
+}
